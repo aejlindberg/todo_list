6a9665b1b9e05e931d051b767ddd76f523b7f23e
Jeanette <aejlindberg@gmail.com>
2018-10-18 16:11:18 +0200

    A working todo app

---
+++ code/src/components/TodoItem.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+class TodoItem extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      done: false
+    }
+  }
+
+handleChecked = () => {
+  const index = this.props.index
+  const done = !this.state.done
+  this.props.updatedTodoItem(index, done)
+
+  this.setState({
+    done: !this.state.done
+  })
+}
+  render() {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          checked={this.props.done}
+          onChange={this.handleChecked} />
+        {this.props.text}
+      </div>
+    )
+  }
+
+}
+
+export default TodoItem
--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,11 +1,55 @@
 import React from "react"
+import TodoItem from "./TodoItem.js"
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      todoItems: [], // store every value which we passing to our todo list
+      inputText: ""
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      inputText: e.target.value
+    })
+  }
+
+  addTodoItem = () => {
+    const currentItems = this.state.todoItems
+    const item = { text: this.state.inputText, done: false }
+    currentItems.push(item)
+    this.setState({
+      todoItems: currentItems
+    })
+  }
+
+  updatedTodoItem = (index, done) => {
+    const currentItems = this.state.todoItems
+    currentItems[index].done = done
+    this.setState({
+      todoItems: currentItems
+    })
+  }
 
   render() {
     return (
       <div>
-        Find me in src/app.js!
+        <form>
+          <input type="text" onChange={this.handleInputChange} />
+          <button type="button" onClick={this.addTodoItem}>Add</button>
+        </form>
+        {this.state.todoItems.map((item, index) => (
+          <TodoItem
+            text={item.text}
+            key={index}
+            index={index}
+            done={item.done}
+            updatedTodoItem={this.updatedTodoItem} />
+        ))}
+
       </div>
     )
   }
